271c8b9bf82abce0909c4bd3688518b0d3b5d06a
Dalibor Kalember <49121432+Kalember@users.noreply.github.com>
2019-12-17 22:46:03 +0100

    I dan

--- cms/php/login.php
+++ cms/php/login.php
@@ -1,1 +1,25 @@
-
+<?php
+session_start();
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'online_shop';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+ $email=$_POST['email'];
+ $pass=$_POST['password'];
+ $select_data=mysql_query("select * from user where email='$email' and password='$pass'");
+ if($row=mysql_fetch_array($select_data))
+ {
+  $_SESSION['email']=$row['email'];
+  echo "success";
+ }
+ else
+ {
+  echo "fail";
+ }
+ exit();
+}
+?>
